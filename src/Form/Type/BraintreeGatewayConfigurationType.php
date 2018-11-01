042e13d99442a07415ba42005f57ae9b2d6290b9
Patryk Drapik <drapikpatryk@gmail.com>
2018-11-01 18:39:41 +0100

    first commit

---
+++ src/Form/Type/BraintreeGatewayConfigurationType.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * another great project.
+ * You can find more information about us on https://bitbag.shop and write us
+ * an email on mikolaj.krol@bitbag.pl.
+ */
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusBraintreePlugin\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+final class BraintreeGatewayConfigurationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('merchantId', TextType::class, [
+                'label' => 'bitbag_sylius_braintree_plugin.ui.merchant_id',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'bitbag_sylius_braintree_plugin.api_key.merchant_id',
+                        'groups' => 'sylius',
+                    ]),
+                ],
+            ])
+            ->add('publicKey', TextType::class, [
+                'label' => 'bitbag_sylius_braintree_plugin.ui.public_key',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'bitbag_sylius_braintree_plugin.public_key.not_blank',
+                        'groups' => 'sylius',
+                    ]),
+                ],
+            ])
+            ->add('privateKey', TextType::class, [
+                'label' => 'bitbag_sylius_braintree_plugin.ui.private_key',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'bitbag_sylius_braintree_plugin.private_key.not_blank',
+                        'groups' => 'sylius',
+                    ]),
+                ],
+            ])
+            ->add('sandbox', CheckboxType::class, [
+                'label' => 'bitbag_sylius_braintree_plugin.ui.sandbox',
+            ])
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+                $data = $event->getData();
+
+                $data['payum.http_client'] = '@sylius.payum.http_client';
+            })
+        ;
+    }
+}
